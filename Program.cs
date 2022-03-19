eeab46c7102c05cd141ddc16a163f9cc3f059840
mohamed aamiin <engmohamedaamiin@gmail.com>
2022-03-18 23:56:50 -0700

    Csharp for vscode

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("welcome my csharp");
+
+
+string variableName = "some text";
+Console.WriteLine("variableis:" + variableName);
+
+
+string name;
+name="muscab  mohamed      ".Trim();
+name = name.Remove(1,5);
+
+
+
+var magac =   "       muha mohamed          ";
+Console.WriteLine("name is " + name + " '");
+Console.WriteLine("name is " + magac + " '");
+
+
+
+var lyric="naftii jamataa ilmaysey";
+var result = lyric.Contains("naftii");
+Console.WriteLine("heestu waa:" + result);
